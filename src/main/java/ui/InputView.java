494c8f7417c74d2c3f420eae699ac18dd38fb71f
LeeYoungJun <dldudwns72@gmail.com>
2021-08-03 22:29:06 +0900

    경주할 자동차 이름 입력

---
+++ src/main/java/ui/InputView.java
@@ -0,0 +1,14 @@
+package ui;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static void main(String[] args) {
+
+        System.out.println("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)를 기준으로 구분.");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
