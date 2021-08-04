18077ba074008dd3f3095fb09d0fb8b0a06052dc
LeeYoungJun <dldudwns72@gmail.com>
2021-08-04 22:12:33 +0900

    입력 방식의 메서드화

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -4,16 +4,21 @@ package domain;
 public class Car {
 
     private String name;
+    private int count;
 
-    public Car(String name){
+    public Car(String name) {
         isValid(name);
-        name = this.name;
+        this.name = name;
     }
 
-    private void isValid(String name){
-        if(name.length() > 5){
+    private void isValid(String name) {
+        if (name.length() > 5) {
             throw new IllegalArgumentException("이름은 5자리를 넘길 수 없습니다.");
         }
+    }
+
+    public String printName(){
+        return name + " : " +"-";
     }
 
 
--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -10,13 +10,30 @@ import java.util.Scanner;
 public class InputView {
     public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)를 기준으로 구분.");
+        String createCar = inputCar();
 
-        Scanner scanner = new Scanner(System.in);
+        String[] split_createCar = createCar.split(",");
 
-        Car car = new Car(scanner.next());
 
+        inputCount();
 
 
     }
+
+    private static String inputCar(){
+        System.out.println("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)를 기준으로 구분).");
+
+        Scanner createCar = new Scanner(System.in);
+
+        return createCar.nextLine();
+    }
+
+    private static int inputCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        Scanner racingCountInput = new Scanner(System.in);
+
+        return racingCountInput.nextInt();
+    }
+
 }
