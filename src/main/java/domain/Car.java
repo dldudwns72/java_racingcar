cb076cb354a5b2ff2f4b09c9fd015a999de7de63
LeeYoungJun <dldudwns72@gmail.com>
2021-08-05 23:59:09 +0900

    Car Class count 입력

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,15 +1,29 @@
 package domain;
+/**
+ * domain이 뜻하는 의미가 무엇일까
+ * =>
+ */
 
+import java.util.Random;
 
+/**
+ * Car가 하는 역할이 뭘까 생각해보자
+ * 1) 이름을 받아서 생성한다. 생성자
+ * 1-1) 이름은 5자리를 넘길 수 없다. isValid
+ * 2) 레이싱을 진행하며 진행 시 마다 전진할지 안할지 판단하여 전진한다
+ * 3) 전진 여부를 파악한 후 이름과 함께 출력한다.
+ */
 public class Car {
 
     private String name;
-    private int input_count;
+    private int go_count = 0;
 
-    public Car(String name,int input_count) {
+    Random random = new Random();
+
+    public Car(String name) {
         isValid(name);
+
         this.name = name;
-        this.input_count = input_count;
     }
 
     private void isValid(String name) {
@@ -18,8 +32,20 @@ public class Car {
         }
     }
 
-    public void printCar(){
-        System.out.println(name + " : " +"-");
+    public void go(int number) {
+        int random_value = random.nextInt(9) + 1;
+
+        if (number < random_value) {
+            go_count++;
+        }
+    }
+
+    public void printCar() {
+        System.out.print(name + " : ");
+
+        for (int i = 0; i < go_count; i++) {
+            System.out.print("-");
+        }
     }
 
     public String getName() {
