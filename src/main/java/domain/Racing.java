dcdab9dd6a39e56dce8770b2cc08beb960434e9c
LeeYoungJun <dldudwns72@gmail.com>
2021-08-11 23:25:50 +0900

    racing 객체를 통한 게임 진행 구현

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Racing {
 
@@ -12,7 +14,29 @@ public class Racing {
         this.attemptCount = attemptCount;
     }
 
-    public void printResult(){
+    public List<Car> start(){
+        List<Car> result = new ArrayList<>();
 
+        System.out.println("attemptCount" + attemptCount);
+        for (int attemptIndex = 0; attemptIndex < attemptCount; attemptIndex++) {
+            Car car = inGame(cars.get(attemptIndex),attemptIndex);
+            result.add(car);
+        }
+
+        return result;
     }
+
+    private Car inGame(Car car, int attemptIndex){
+
+        for (int i = 0; i < attemptIndex+1; i++) {
+            System.out.println("attemptIndex" + attemptIndex);
+
+            int randomNo = new Random().nextInt(10);
+            car.move(randomNo);
+        }
+
+        return car;
+    }
+
+
 }
