67efbe2d6bc168016ef65755c94b6224d9c0367f
LeeYoungJun <dldudwns72@gmail.com>
2021-08-13 00:41:55 +0900

    이름 입력 제한 Exception 클래스 생성 및 적용

---
+++ src/main/java/Exceptions/NameValidationException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class NameValidationException extends IllegalArgumentException{
+
+    public NameValidationException(String message){
+        super(message);
+    }
+
+
+}
--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,22 +1,12 @@
 package domain;
 
 import java.util.List;
-import java.util.Random;
 
 public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
-    }
-
-    public void move(int attempt_count) {
-        for (int i = 0; i < cars.size(); i++) {
-            int randomNo = new Random().nextInt(10);
-            Car car = cars.get(i);
-            car.move(randomNo);
-        }
-        System.out.println("");
     }
 
     @Override
