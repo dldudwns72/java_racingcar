01880ee41f6c2de5a1ee83cb74594f33e8a5c0c4
LeeYoungJun <dldudwns72@gmail.com>
2021-08-13 23:39:29 +0900

    car test 구현

--- src/test/java/domainTest/CarTest.java
+++ src/test/java/domainTest/CarTest.java
@@ -1,10 +1,12 @@
 package domainTest;
 
+import Exceptions.NameValidationException;
 import domain.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
@@ -14,9 +16,8 @@ public class CarTest {
         Car car = new Car("lee");
 
         car.move(6);
-        car.move(6);
 
-        assertThat(car.getMoveCount()).isEqualTo(2);
+        assertThat(car.getMoveCount()).isEqualTo(1);
     }
 
     @Test
@@ -24,9 +25,41 @@ public class CarTest {
     public void 정지(){
         Car car = new Car("lee");
 
-        car.move(4);
+        car.move(2);
 
         assertThat(car.getMoveCount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("두칸 전진")
+    public void two전진(){
+        Car car = new Car("lee");
+
+        car.move(6);
+        car.move(6);
+
+        assertThat(car.getMoveCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("세번 실행 2칸전진 1칸 정지")
+    public void two전진one정지(){
+        Car car = new Car("lee");
+
+        car.move(6);
+        car.move(6);
+        car.move(2);
+
+        assertThat(car.getMoveCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("5자 이상 에러")
+    public void nameValid(){
+        assertThatThrownBy(()->{
+            Car car = new Car("leekim");
+        }).isInstanceOf(NameValidationException.class);
+
     }
 
 
