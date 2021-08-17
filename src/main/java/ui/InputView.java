7a33575b1501ad2920a60145060c97628a7fc70e
yjlee <yjlee@dfocus.net>
2021-08-17 22:37:31 +0900

    inputView 메소드 기능별 정리

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -2,30 +2,39 @@ package ui;
 
 
 import domain.Car;
-import domain.Cars;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner input = new Scanner(System.in);
 
     public static List<Car> start(){
+
+        List<String> carNames = inputCars();
+
+        List<Car> carList = createCars(carNames);
+
+        return carList;
+    }
+
+    // static 메소드로 선언해야 하는 이유?
+    private static List<String> inputCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         Scanner scanner = new Scanner(System.in);
 
         String inputNames = scanner.nextLine();
 
-        List<String> splitInputNames = Arrays.asList(inputNames.split(","));
+        return Arrays.asList(inputNames.split(","));
+    }
 
+    private static List<Car> createCars(List<String> cars){
         List<Car> carList = new ArrayList<>();
 
-        for (int i = 0; i < splitInputNames.size(); i++) {
-            carList.add(new Car(splitInputNames.get(i)));
+        for (int i = 0; i < cars.size(); i++) {
+            carList.add(new Car(cars.get(i)));
         }
 
         return carList;
