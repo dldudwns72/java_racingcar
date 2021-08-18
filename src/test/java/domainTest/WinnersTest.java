af34a0a3111e2778f3ef75727067f0948364895c
LeeYoungJun <dldudwns72@gmail.com>
2021-08-18 23:59:26 +0900

    winnersTest class 추가

--- src/test/java/domainTest/WinnersTest.java
+++ src/test/java/domainTest/WinnersTest.java
@@ -1,2 +1,13 @@
-package domainTest;public class WinnersTest {
+package domainTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinnersTest {
+
+    @Test
+    @DisplayName("우승자 판별")
+    public void judge(){
+
+    }
 }
